8d8d0342ea1be5a5e5c0a3d9d43cb6435979dc0b
SURIYA M <136674995+Suriya-18@users.noreply.github.com>
2025-07-23 06:34:20 +0530

    Create Protocol – Entanglement-Based QKD.py

---
+++ Protocol – Entanglement-Based QKD.py
@@ -0,0 +1,11 @@
+from qiskit import QuantumCircuit, Aer, execute
+
+qc = QuantumCircuit(2, 2)
+qc.h(0)
+qc.cx(0, 1)
+qc.measure([0, 1], [0, 1])
+
+backend = Aer.get_backend('qasm_simulator')
+result = execute(qc, backend, shots=1000).result()
+counts = result.get_counts()
+print("E91 Measurement Results:", counts)
